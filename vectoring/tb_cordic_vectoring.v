9aaa44254796725461c847f2105098d29ee03fd2
Chandu <ee14btech11014@iith.ac.in>
2018-01-22 10:11:43 +0530

    Update tb_cordic_vectoring.v

--- vectoring/tb_cordic_vectoring.v
+++ vectoring/tb_cordic_vectoring.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/06/2017 08:30:12 AM
-// Design Name: 
-// Module Name: tb_cordic_vectoring
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_cordic_vectoring();
     parameter data_width = 16;
